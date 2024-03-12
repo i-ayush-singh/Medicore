24e1366418e08ddd4e1aacb0fe13a3b61f4f5db4
ArchitKB <architkumarbhardwaj15@gmail.com>
2024-03-12 23:58:24 +0530

    requests fully functioning

--- Backend/controllers/doctor.js
+++ Backend/controllers/doctor.js
@@ -30,3 +30,26 @@ export const getDoctor = async (req, res) => {
     })),
   });
 };
+
+export const handleRequest = async (req, res) => {
+  try {
+    const { result, doctorId } = req.body;
+    const { patientId } = req.params;
+
+    const doctor = await Doctor.findById(doctorId);
+
+    const index = doctor.requests.indexOf(patientId);
+
+    doctor.requests.splice(index, 1);
+
+    if (result === "true") {
+      doctor.patientList.push(patientId);
+    }
+
+    await doctor.save();
+
+    res.status(200).json(doctor);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
--- backend/routes/doctor.js
+++ backend/routes/doctor.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getDoctor } from "../controllers/doctor.js";
+import { getDoctor, handleRequest } from "../controllers/doctor.js";
 import { verifyToken } from "../middleware/auth.js";
 const router = express.Router();
 
 router.get("/fetchAll", verifyToken, getDoctor);
+router.patch("/request/:patientId", verifyToken, handleRequest);
 
 export default router;
