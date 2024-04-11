e6baa72ae51b5979dc56cd7ddaee8e10e15c1e91
i-ayush-singh <145232150+i-ayush-singh@users.noreply.github.com>
2024-04-11 20:12:45 +0530

    Create patient.js

---
+++ Backend/controllers/patient.js
@@ -0,0 +1,84 @@
+import Doctor from "../models/Doctor.js";
+import Patient from "../models/Patient.js";
+
+export const requestAppointment = async (req, res) => {
+  try {
+    const { patientId } = req.body;
+    const { doctorId } = req.params;
+
+    const doctor = await Doctor.findById(doctorId);
+
+    doctor.requests.push(patientId);
+
+    doctor.save();
+
+    res.status(200).json(doctor);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+export const bookAppointment = async (req, res) => {
+  try {
+    const { doctorId } = req.params;
+    const { patientId, time, date } = req.body;
+
+    const doctor = await Doctor.findById(doctorId);
+    const patient = await Patient.findById(patientId);
+
+    if (doctor.patientList.indexOf(patientId) == -1) {
+      return res.status(407).json({
+        error: "It seems you've not been assigned to this doctor yet",
+      });
+    }
+    const { timings } = doctor;
+
+    const patientTime = parseInt(time);
+    const startTime = parseInt(timings[0]),
+      endTime = parseInt(timings[1]);
+
+    if (patientTime >= endTime || patientTime < startTime) {
+      const message = `Sorry, Dr. ${doctor.fullName} is not available at that time`;
+
+      patient.notifications.push(message);
+    } else {
+      const message = `Dr. ${doctor.fullName} has been informed  of your request :)`;
+
+      patient.notifications.push(message);
+
+      const request = {
+        date,
+        time,
+        patientId,
+      };
+
+      doctor.appointmentRequests.push(request);
+
+      await doctor.save();
+    }
+
+    await patient.save();
+
+    res.status(200).json(doctor);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+
+export const handleNotifications = async (req, res) => {
+  try {
+    const { message, patientId } = req.body;
+
+    const patient = await Patient.findById(patientId);
+
+    const index = patient.notifications.indexOf(message);
+
+    notifications.splice(index, 1);
+
+    await patient.save();
+
+    res.status(200).json(patient);
+  } catch (error) {
+    res.status(409).json({ error: error.message });
+  }
+};
