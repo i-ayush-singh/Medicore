b0208ab8649f83afccea074472c37e10a1c0fe09
Aayushdhingra09 <127206240+Aayushdhingra09@users.noreply.github.com>
2024-04-11 20:16:35 +0530

    Create Patient.js
    Create Patient.js

---
+++ Backend/models/Patient.js
@@ -0,0 +1,61 @@
+import mongoose from "mongoose";
+const PatientSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    blood: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: String,
+      required: true,
+    },
+    sex: {
+      type: String,
+      required: true,
+    },
+    doctorList: {
+      type: Array,
+      default: [],
+    },
+    notifications: {
+      type: Array,
+      default: [],
+    },
+    files: {
+      type: Map,
+      of: String,
+    },
+    appointments: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+const Patient = mongoose.model("Patient", PatientSchema);
+export default Patient;
