c820d2b9aea5153dc943592b18ad412f2fe22072
BishalKedia <bishalkediadgp@gmail.com>
2024-04-11 23:08:26 +0530

    Login styling updated

--- frontend/src/Pages/LoginP.jsx
+++ frontend/src/Pages/LoginP.jsx
@@ -66,27 +66,32 @@ export function LoginP(){
     };
 
     return(
-        <div>
-            <form onSubmit={formsubmit}>
-            
-                <input type ="email" name = "email" placeholder = "enter your email" value = {formDetails.email} onChange = {inputChange} />
-                <input
+        <section className="bg-slate-300 h-screen flex justify-center w-full">
+            <div onSubmit={formsubmit} className="flex flex-col justify-center rounded-lg bg-white w-1/3 text-center px-8 py-0 h-3/4 mt-20 mb-20">
+            <div class="font-bold text-4xl py-10">Login</div>
+            <div className="px-4 py-4">
+                <input type ="email" name = "email" placeholder = "Enter your Email" value = {formDetails.email} onChange = {inputChange} class="w-full px-2 py-1 border rounded border-slate-200"/></div>
+                <div className="px-4 py-4"><input
             type="password"
             name="password"
             placeholder="Enter your password"
             value={formDetails.password}
             onChange={inputChange}
-          />
-        <select value= {tt} onChange={typeChange}>
-                <option value="doctor">doctor</option>
-                <option value="patient">patient</option>
-        </select>
+            class="w-full px-2 py-1 border rounded border-slate-200"
+          /></div>
+          <div className="px-4 py-4">
+        <select value= {tt} onChange={typeChange} class="w-full px-2 py-1 border rounded border-slate-200">
+                <option value="doctor">Doctor</option>
+                <option value="patient">Patient</option>
+        </select></div>
+        <div className="px-4 py-4">
         <button
             type="submit"
+            class="w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
           >
-            sign in
-          </button>
-            </form>
-        </div>
+            Sign in
+          </button></div>
+            </div>
+        </section>
     )
 }
