8bc49696bb64c867cd475f338dbe88848109f833
i-ayush-singh <singh.ayush.6262@gmail.com>
2024-04-13 12:09:05 +0530

    landing updated

--- frontend/src/Pages/Landing/Header.jsx
+++ frontend/src/Pages/Landing/Header.jsx
@@ -17,15 +17,13 @@ export function Header() {
        </div>
        </div>
      <div className="w-1/3  inline-flex gap-4  justify-end">
-     <Link to="/login">
-        <button  >
-            login
-        </button>
-        </Link>
         <div>
         <Link to="/signup/doctor">
         <button  className="inline-flex py-2 px-3.5 me-2 mb-2 text-xs font-medium text-grey-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-700 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-grey-700">
-        <UserCircleIcon className="h-5 w-5 mr-2" aria-hidden="true" /> Sign up</button></Link>
+        <UserCircleIcon className="h-5 w-5 mr-2" aria-hidden="true" />Doctor's Signup</button></Link>
+        <Link to="/signup/patient">
+        <button  className="inline-flex py-2 px-3.5 me-2 mb-2 text-xs font-medium text-grey-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-700 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-grey-700">
+        <UserCircleIcon className="h-5 w-5 mr-2" aria-hidden="true" />Patient's Signup</button></Link>
         </div>
      </div>
     </div>
