3ed70d7966a4d65bd8705ff7f23e273b86b03a3f
BishalKedia <bishalkediadgp@gmail.com>
2024-04-13 12:10:49 +0530

    Doctors Page Styling Done

--- frontend/src/Components/doctorcard.jsx
+++ frontend/src/Components/doctorcard.jsx
@@ -33,14 +33,14 @@ export const DoctorCard = ({ ele }) => {
 
 
   return(   
-<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <img class="p-8 rounded-t-lg" src={`http://localhost:3001/assets/${ele.picturePath}`} alt="doctor" />
+<div className="w-full max-w-sm bg-slate-400 border border-gray-200 rounded-lg shadow dark:border-gray-700">
+        <img class="p-8 rounded-t-lg w-full h-56" src={`http://localhost:3001/assets/${ele.picturePath}`} alt="doctor" />
     <div class="px-5 pb-5">
         <a href="#">
-            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{ele.fullName.toUpperCase()}</h5>
+            <h5 class="text-xl font-semibold tracking-tight text-black-700 dark:text-white">{ele.fullName.toUpperCase()}</h5>
         </a>
         <a href="#">
-            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{ele.specialist.toUpperCase()}</h5>
+            <h5 class="text-xl font-semibold tracking-tight text-black-700 dark:text-white">{ele.specialist.toUpperCase()}</h5>
         </a>
         <div class="flex items-center mt-2.5 mb-5">
             <div class="flex items-center space-x-1 rtl:space-x-reverse">
@@ -60,10 +60,10 @@ export const DoctorCard = ({ ele }) => {
                     <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
                 </svg>
             </div>
-            <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">5.0</span>
+            <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">4.0</span>
         </div>
         <div class="flex items-center justify-between">
-            <span class="text-3xl font-bold text-gray-900 dark:text-white">FEES: ₹ {ele.fee}</span>
+            <span class="text-1xl font-bold text-gray-900 dark:text-white">FEES: ₹ {ele.fee}</span>
         </div>
     </div>
     </div>
