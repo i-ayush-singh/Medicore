3ac4da45d30ac1435881d3c5e95af42d803fca38
BishalKedia <bishalkediadgp@gmail.com>
2024-04-13 11:03:02 +0530

    ...

--- frontend/src/Components/doctorcard.jsx
+++ frontend/src/Components/doctorcard.jsx
@@ -33,17 +33,14 @@ export const DoctorCard = ({ ele }) => {
 
 
   return(   
-
 <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
         <img class="p-8 rounded-t-lg" src={`http://localhost:3001/assets/${ele.picturePath}`} alt="doctor" />
-    </a>
     <div class="px-5 pb-5">
         <a href="#">
             <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{ele.fullName.toUpperCase()}</h5>
         </a>
         <a href="#">
-            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{ele.specialist}</h5>
+            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{ele.specialist.toUpperCase()}</h5>
         </a>
         <div class="flex items-center mt-2.5 mb-5">
             <div class="flex items-center space-x-1 rtl:space-x-reverse">
@@ -70,7 +67,6 @@ export const DoctorCard = ({ ele }) => {
         </div>
     </div>
     </div>
-
   )
 
 
