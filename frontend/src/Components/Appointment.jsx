b2188ce72334d5340e71a5a95b565338d605abad
i-ayush-singh <singh.ayush.6262@gmail.com>
2024-04-14 22:53:57 +0530

    Book Appointment added

--- frontend/src/Components/Appointment.jsx
+++ frontend/src/Components/Appointment.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react';
+import React,{useState} from 'react';
+import "bootstrap";
 import {Button,Form,Modal} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+export function Appointment(props){
 
+  const [formDetails, setFormDetails] = useState({
+    date: "",
+    time: "",
+  });
+  console.log(props.userId)
+  console.log(props.doctorId)
+const inputChange = (e) => {
+    const { name, value } = e.target;
+     return setFormDetails({
+      ...formDetails,
+      [name]: value,
+    });
+};
+const handleClose = () => props.setShow(false);
+async function BookAppointment(){
+  const userId = props.userId;
+  const doctorId = props.doctorId;
+  console.log(userId)
+  try{
+    const response = await axios.patch(`http://localhost:3001/patient/booking/${doctorId}`,
+    {
+      patientId : userId,
+           time : formDetails.time[0]+formDetails.time[1]+formDetails.time[3]+formDetails.time[4],
+           date : formDetails.date
+    },
+    {
+      headers : {
+        'Authorization': "Bearer " +localStorage.getItem('token').slice(1,-1),
+        'Content-Type' : 'application/json',
+    }
+   })
+   console.log(response.data);
+  }catch(err){
+    console.log(err);
+  }
 
-export function Appointment(props) {
-    const [formDetails, setFormDetails] = useState({
-        date: "",
-        time: "",
-      });
-    const inputChange = (e) => {
-        const { name, value } = e.target;
-        return setFormDetails({
-          ...formDetails,
-          [name]: value,
-        });
-    };
-  const handleClose = () => props.setShow(false);
-  
-
-  return (
-    <>
-      <Modal show={props.show} onHide={handleClose}>
+}
+    return(
+        <Modal show={props.show} onHide={handleClose} size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Create Post</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+        <Form>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Label>date</Form.Label>
               <Form.Control
                 type="date"
+                name = "date"
                 placeholder="select your date"
+                value = {formDetails.date}
                 onChange = {inputChange}
                 autoFocus
               />
@@ -38,19 +66,17 @@ export function Appointment(props) {
               className="mb-3"
             >
               <Form.Label>select time</Form.Label>
-              <Form.Control type = "time"  placeholder="select your time" onChange = {inputChange}/>
+              <Form.Control value = {formDetails.time} name = "time" type = "time"  placeholder="select your time" onChange = {inputChange}/>
             </Form.Group>
           </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+        <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" >
+          <Button variant="primary" onClick={BookAppointment} >
             Book appointment
           </Button>
         </Modal.Footer>
-      </Modal>
-    </>
-  );
+      </Modal>);
 }
