718cb2409791e52a7905f0e848bb8f98a39691ca
ArchitKB <architkumarbhardwaj15@gmail.com>
2024-04-17 23:28:36 +0530

    backend modifications

--- frontend/src/Pages/ChatTest.jsx
+++ frontend/src/Pages/ChatTest.jsx
@@ -1,14 +1,34 @@
-import React from "react";
-import io from "socket.io-client";
+// import { useEffect } from "react";
 
+import React, { createContext, useMemo, useContext } from "react";
+import { io } from "socket.io-client";
+
+const SocketContext = createContext(null);
+
+const useSocket = () => {
+  const socket = useContext(SocketContext);
+  return socket;
+};
+
+const SocketProvider = (props) => {
+  const socket = useMemo(() => io("localhost:4000"), []);
+
+  return (
+    <SocketContext.Provider value={socket}>
+      {props.children}
+    </SocketContext.Provider>
+  );
+};
 
 export const ChatTest = () => {
-    const socket = io('http://localhost:4000');
+    const socket = useSocket();
 
+    console.log(socket);
     const emitter = () => {
         console.log("here");
         socket.emit('connection');
     }
+
     return(
         <p onClick={emitter}>Hello World</p>
     );
